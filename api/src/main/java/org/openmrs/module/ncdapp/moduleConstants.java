5ad6a88bf6486c5736259988384723ee06d502cd
RAJABIBRAZ <akilkil5@gmail.com>
2023-04-28 12:48:14 +0300

    second commit

---
+++ api/src/main/java/org/openmrs/module/ncdapp/moduleConstants.java
@@ -0,0 +1,10 @@
+package org.openmrs.module.ncdapp;
+
+public class moduleConstants {
+	
+	public static final String MODEL_ATTR_CURRENT_PATIENT = "currentPatient";
+	
+	public static final String MODULE_ID = "ncd";
+	
+	public static final String NCD_APP_ID = "ncd.app.home";
+}
